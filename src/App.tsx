fd2dd6a16054716668fc7780701322d8fd63a06a
rhtjddls123 <jjjk0605@naver.com>
2025-04-16 17:35:16 +0900

    feat: react-router, tanstack-query Provider설정
    tanstack-query에서 사용할 queryClient는 utils/queryClient에서 export하여
    추후 cache관련 용도로 사용될 수 있도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { queryClient } from "./utils/queryClient";
+
+const router = createBrowserRouter([{ path: "/" }]);
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
