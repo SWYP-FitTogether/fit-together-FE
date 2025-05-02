b0ffb3da5f93353e1dc620c0922e7c08b2f38c7c
SeongIn Ko <jjjk0605@naver.com>
2025-05-02 19:36:35 +0900

    feat: 커스텀 컴포넌트 ProgressBar 컴포넌트 추가
    - ProgressBar 컴포넌트 구현
    - shadcn의 progress를 사용하여 ProgressBar 공통 컴포넌트 개발
    - withIndicator가 true일 경우 ProgressBar의 상세를 보여주도록 구현
    - value와 max를 이용하여 전체 %비율대로 Progress가 설정되도록 구현

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import { Progress } from "./ui/progress";
+import Indicator from "./Indicator";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+interface IProgressBarProps extends React.ComponentProps<typeof ProgressPrimitive.Root> {
+  value: number;
+  max: number;
+  withIndicator?: boolean;
+  className?: string;
+}
+
+const ProgressBar = ({
+  withIndicator = false,
+  value,
+  max,
+  className,
+  ...props
+}: IProgressBarProps) => {
+  return (
+    <div className={cn("flex flex-col gap-0.5 w-[335px] items-end", className)}>
+      <Progress
+        className="h-3 bg-gray-100 shadow-inset [&>div]:bg-main-primary [&>div]:shadow-inset"
+        value={(value / max) * 100}
+        {...props}
+      />
+      {withIndicator && <Indicator startNum={value} endNum={max} />}
+    </div>
+  );
+};
+
+export default ProgressBar;
