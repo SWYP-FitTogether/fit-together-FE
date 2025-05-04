811d660bb96f2102f987f026fe42314077675c67
SeongIn Ko <jjjk0605@naver.com>
2025-05-04 14:15:27 +0900

    feat: 커스텀 컴포넌트 CheckBox 구현
    - CheckBox컴포넌트 구현
    - 공통 컴포넌트 구현
    - label에 따라 총 2가지 버전으로 동작하도록 구현
    - 기본 스타일과 select, disabled스타일 각각 구현

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import { Checkbox } from "./ui/checkbox";
+import { Label } from "./ui/label";
+
+interface ICheckBoxProps extends React.ComponentProps<typeof Checkbox> {
+  id: string;
+  label?: string;
+}
+
+const CheckBox = ({ id, label, ...props }: ICheckBoxProps) => {
+  return (
+    <div className="py-2 px-3 flex items-center w-fit h-11 gap-1">
+      <div className="w-6 h-6 flex justify-center items-center p-[3px]">
+        <Checkbox
+          id={id}
+          className="w-[18px] h-[18px] border-[1.5px] border-gray-400 data-[state=checked]:border-none data-[state=checked]:bg-main-primary disabled:bg-gray-200 disabled:border-none"
+          {...props}
+        />
+      </div>
+      {label && (
+        <Label
+          htmlFor={id}
+          className={cn("text-body-1 text-gray-700 h-[26px]", props.disabled && "text-gray-400")}
+        >
+          {label}
+        </Label>
+      )}
+    </div>
+  );
+};
+
+export default CheckBox;
