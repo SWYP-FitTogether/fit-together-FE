afe635d43edf64ea7b84b14592526b36b33c8088
SeongIn Ko <jjjk0605@naver.com>
2025-05-04 18:07:04 +0900

    feat: 커스텀 컴포넌트 chipButton 구현
    - SingleChipButton, MultiChipButton 컴포넌트 구현
    - 공통 컴포넌트 개발
    - shadcn checkbox, radio-group컴포넌트를 커스텀하여 개발

---
+++ src/components/SingleChipButton.tsx
@@ -0,0 +1,31 @@
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+function SingleChipGroup({
+  className,
+  ...props
+}: React.ComponentProps<typeof RadioGroupPrimitive.Root>) {
+  return <RadioGroupPrimitive.Root data-slot="radio-group" className={className} {...props} />;
+}
+
+interface ISingleChipButtonProps extends React.ComponentProps<typeof RadioGroupPrimitive.Item> {
+  children: ReactNode;
+}
+
+function SingleChipButton({ children, className, ...props }: ISingleChipButtonProps) {
+  return (
+    <RadioGroupPrimitive.Item
+      data-slot="radio-group-item"
+      className={cn(
+        "w-fit h-16 px-8 py-5 text-body-1 font-semibold bg-gray-white hover:bg-gray-100 text-gray-600 cursor-pointer border border-gray-200 rounded-base data-[state=checked]:bg-main-primary data-[state=checked]:border-main-primary data-[state=checked]:text-gray-black disabled:border-gray-200 disabled:bg-gray-200 disabled:text-gray-400",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </RadioGroupPrimitive.Item>
+  );
+}
+
+export { SingleChipGroup, SingleChipButton };
