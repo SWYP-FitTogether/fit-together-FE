aa8d6d7369236193055cf466050dfc3959760751
SeongIn Ko <jjjk0605@naver.com>
2025-05-04 17:05:45 +0900

    feat: 커스텀 컴포넌트 Dropdown 구현
    - Dropdwon컴포넌트 구현
    - 공통 컴포넌트 개발
    - shadcn Select컴포넌트를 커스텀하여 개발
    - type.ts에 IDropdownItem타입 설정

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,81 @@
+import { cn } from "@/lib/utils";
+import { Label } from "./ui/label";
+import { ScrollArea } from "./ui/scroll-area";
+import { Select, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import { IDropdownItem } from "@/types/type";
+
+function SelectContent({
+  className,
+  children,
+  position = "popper",
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Content>) {
+  return (
+    <SelectPrimitive.Portal>
+      <SelectPrimitive.Content
+        data-slot="select-content"
+        className={cn(
+          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 max-h-(--radix-select-content-available-height) min-w-[8rem] origin-(--radix-select-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-md border shadow-md",
+          position === "popper" &&
+            "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
+          className
+        )}
+        position={position}
+        {...props}
+      >
+        <SelectPrimitive.Viewport
+          className={cn(
+            "p-1",
+            position === "popper" &&
+              "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1"
+          )}
+        >
+          {children}
+        </SelectPrimitive.Viewport>
+      </SelectPrimitive.Content>
+    </SelectPrimitive.Portal>
+  );
+}
+
+interface IDropdownProps extends React.ComponentProps<typeof Select> {
+  id: string;
+  label?: string;
+  placeholder?: string;
+  items: IDropdownItem[];
+}
+
+const Dropdown = ({ id, placeholder, label, items, ...props }: IDropdownProps) => {
+  return (
+    <div className="flex flex-col gap-1">
+      {label && (
+        <Label htmlFor={id} className="h-[19px] text-caption-1 text-gray-600">
+          {label}
+        </Label>
+      )}
+      <Select {...props}>
+        <SelectTrigger
+          id={id}
+          className="w-full text-body-1 data-[placeholder]:text-gray-500 data-[size=default]:h-[58px] rounded-base border border-gray-200 bg-gray-white p-4 [&>svg]:w-6 [&>svg]:h-6 [&>svg]:text-gray-600 [&>svg]:opacity-1000 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 focus-visible:border-main-secondary data-[state=open]:border-main-secondary focus:data-[state=open]:border-main-secondary data-[state=open]:[&>svg]:rotate-180"
+        >
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          <ScrollArea className="max-h-[232px]">
+            {items.map((item) => (
+              <SelectItem
+                key={item.value}
+                className="h-[58px] p-4 text-body-1 text-gray-400 focus:bg-gray-100 focus:text-gray-700 focus:font-semibold data-[state=checked]:bg-gray-100 data-[state=checked]:text-base data-[state=checked]:font-semibold data-[state=checked]:text-gray-700"
+                value={item.value}
+              >
+                {item.title}
+              </SelectItem>
+            ))}
+          </ScrollArea>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default Dropdown;
