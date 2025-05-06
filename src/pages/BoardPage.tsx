8994db4c92b61fc4b871a164b028afa89670f78e
SeongIn Ko <jjjk0605@naver.com>
2025-05-06 18:28:15 +0900

    feat: board페이지 Navigation 컴포넌트 분리
    - board페이지 Navigation 컴포넌트 분리

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,14 +1,10 @@
-import { MainNavigation } from "@/components/Navigation";
-import ProgressBar from "@/components/ProgressBar";
+import BoardNavigation from "@/features/board/BoardNavigation";
 import BoardTabs from "@/features/board/BoardTabs";
 
 const BoardPage = () => {
   return (
     <div>
-      <MainNavigation type="main" level={0} />
-      <div className="flex justify-center items-center w-full py-2">
-        <ProgressBar withIndicator max={200} value={80} />
-      </div>
+      <BoardNavigation />
 
       <div>
         <BoardTabs />
