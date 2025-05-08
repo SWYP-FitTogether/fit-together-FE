5788c1a8676dc12fb37e90dd501b2a87136be44e
SeongIn Ko <jjjk0605@naver.com>
2025-05-08 10:40:47 +0900

    feat: 공통 컴포넌트 CircleImg 속성 추가
    - M size 추가
    - isSelected, isLocked를 통해 스타일 추가

--- components/CircleImg.tsx
+++ components/CircleImg.tsx
@@ -4,12 +4,15 @@ import { cn } from "@/lib/utils";
 import { HTMLAttributes } from "react";
 import ImageIcon from "./icons/ImageIcon";
 import Image from "next/image";
+import LockIcon from "./icons/LockIcon";
 
 interface ICircleImgProps extends HTMLAttributes<HTMLImageElement> {
-  size: "L" | "S";
+  size: "L" | "M" | "S";
   imgSrc?: string;
   imgAlt?: string;
   isLoading?: boolean;
+  isSelected?: boolean;
+  isLocked?: boolean;
 }
 
 const CircleImg = ({
@@ -17,6 +20,8 @@ const CircleImg = ({
   imgSrc,
   imgAlt,
   isLoading,
+  isSelected,
+  isLocked,
   ...props
 }: ICircleImgProps) => {
   return (
@@ -24,11 +29,16 @@ const CircleImg = ({
       className={cn(
         size === "L" && "h-[72px] w-[72px]",
         size === "S" && "h-9 w-9",
+        size === "M" && "h-[52px] w-[52px]",
         "relative overflow-hidden rounded-full border border-gray-200",
         isLoading && "flex items-center justify-center bg-gray-100",
+        isSelected && "border-main-secondary shadow-drop",
+        isLocked &&
+          "flex cursor-not-allowed items-center justify-center bg-bk-opacity-50",
       )}
+      onClick={!isLocked ? props.onClick : undefined}
     >
-      {!isLoading && (
+      {!isLoading && !isLocked && (
         <Image
           src={imgSrc || "/blankImg.png"}
           alt={imgAlt || "공백 이미지"}
@@ -37,11 +47,22 @@ const CircleImg = ({
           fill
         />
       )}
-      {isLoading && (
+      {isLoading && !isLocked && (
         <ImageIcon
           className={cn(
             "text-gray-300",
             size === "L" && "h-6 w-6",
+            size === "M" && "h-5 w-5",
+            size === "S" && "h-4 w-4",
+          )}
+        />
+      )}
+      {isLocked && (
+        <LockIcon
+          className={cn(
+            "text-gray-300",
+            size === "L" && "h-6 w-6",
+            size === "M" && "h-5 w-5",
             size === "S" && "h-4 w-4",
           )}
         />
