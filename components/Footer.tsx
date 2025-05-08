dc2dbd829cbd41e13a57cdcae48246ccd0cd6e71
SeongIn Ko <jjjk0605@naver.com>
2025-05-08 14:07:07 +0900

    feat: 커스텀 컴포넌트 Footer 구현

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import { Divider } from "./Divider";
+import TertiaryButton from "./TertiaryButton";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center gap-1 p-5">
+      <div className="flex items-center gap-2">
+        <TertiaryButton>로그아웃</TertiaryButton>
+        <Divider orientation="vertical" className="h-4" />
+        <TertiaryButton>공지사항</TertiaryButton>
+        <Divider orientation="vertical" className="h-4" />
+        <TertiaryButton>FAQ</TertiaryButton>
+      </div>
+      <p className="text-caption-2 text-gray-600">© 2025 Fit Together</p>
+    </footer>
+  );
+};
+
+export default Footer;
