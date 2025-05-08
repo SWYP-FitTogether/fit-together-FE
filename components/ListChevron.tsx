fe44039f5068767c81e6a61d216342fcfee824eb
SeongIn Ko <jjjk0605@naver.com>
2025-05-08 10:23:22 +0900

    feat: 공통 컴포넌트 ListChevron에 label추가
    - label을 옵셔널로 받아 표현

--- components/ListChevron.tsx
+++ components/ListChevron.tsx
@@ -5,15 +5,17 @@ import NextIcon from "./icons/NextIcon";
 interface IListChevronProps {
   title: string;
   onClick?: () => void;
+  label?: string;
 }
 
-const ListChevron = ({ title, onClick }: IListChevronProps) => {
+const ListChevron = ({ title, label, onClick }: IListChevronProps) => {
   return (
     <div
       className="flex h-16 cursor-pointer items-center gap-2 rounded-base border border-gray-200 bg-gray-white px-4 py-5 hover:shadow-drop active:shadow-drop"
       onClick={onClick}
     >
       <p className="h-[22px] grow text-headline-3 text-gray-700">{title}</p>
+      {label && <p className="text-caption-1 text-main-secondary">{label}</p>}
       <NextIcon className="h-6 w-6 text-gray-500" />
     </div>
   );
