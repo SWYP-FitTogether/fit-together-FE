1000b0a4fc774c09ca1a5e076997b9f496174606
SeongIn Ko <jjjk0605@naver.com>
2025-05-17 15:37:51 +0900

    feat: 로그아웃 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,14 @@
+"use client";
+import { useLogout } from "@/hooks/useAuth";
 import { Divider } from "./Divider";
 import TertiaryButton from "./TertiaryButton";
 
 const Footer = () => {
+  const { mutate } = useLogout();
   return (
     <footer className="flex flex-col items-center gap-1 bg-gray-100 p-5">
       <div className="flex items-center gap-2">
-        <TertiaryButton>로그아웃</TertiaryButton>
+        <TertiaryButton onClick={() => mutate()}>로그아웃</TertiaryButton>
         <Divider orientation="vertical" className="h-4" />
         <TertiaryButton>공지사항</TertiaryButton>
         <Divider orientation="vertical" className="h-4" />
