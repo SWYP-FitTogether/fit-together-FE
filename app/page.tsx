4ae74eba7439127e8faa30ad0fe142f2b3daf141
SeongIn Ko <jjjk0605@naver.com>
2025-05-24 22:36:05 +0900

    feat: 라우팅 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { useAuthStore } from "@/store/useAuthStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const router = useRouter();
+  const { accessToken } = useAuthStore();
+
+  useEffect(() => {
+    if (!accessToken) {
+      router.replace("/login");
+    } else {
+      router.replace("/board");
+    }
+  }, [router, accessToken]);
+
+  return null;
 }
